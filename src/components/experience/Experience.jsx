c484571c28ac8b03e451d7e68afda4e52628b8ec
R ABHISHEK REDDY <93172671+lordprime@users.noreply.github.com>
2024-06-25 20:12:48 +0530

    Update Experience.jsx

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -5,7 +5,7 @@ import { BsPatchCheckFill } from 'react-icons/bs';
 const Experience = () => {
     return (
         <section id='experience'>
-            <h5> Skills I Have</h5>
+            <h5>Skills I Have</h5>
             <h2>My Experience</h2>
 
             <div className="container experience__container">
@@ -17,13 +17,12 @@ const Experience = () => {
                             <BsPatchCheckFill className='experience__details-icon' />
                             <div>
                                 <h4>Vulnerability Assessment</h4>
-                                <small className='text-light'>Conducted vulnerability assessments and penetration tests on web applications, network 						infrastructure and android.</small>
- 				<small className='text-light'>Mastered tools like Burp Suite, Metasploit, Nmap to access and analyze target systems.</small>
-			        <small className='text-light'>Successfully identified and reported an high security Vuln in web application. Performed automated security testing and worked in configuration of security testing with custom scripts.</small>
+                                <small className='text-light'>Conducted vulnerability assessments and penetration tests on web applications, network infrastructure and android.</small>
+                                <small className='text-light'>Mastered tools like Burp Suite, Metasploit, Nmap to access and analyze target systems.</small>
+                                <small className='text-light'>Successfully identified and reported a high security vuln in a web application. Performed automated security testing and worked in configuration of security testing with custom scripts.</small>
                             </div>
                         </article>
                     </div>
-                </div>
                 </div>
 
                 {/* Programming Languages */}
@@ -91,6 +90,7 @@ const Experience = () => {
                                 <small className='text-light'>Experienced</small>
                             </div>
                         </article>
+
                         <article className='experience__details'>
                             <BsPatchCheckFill className='experience__details-icon' />
                             <div>
@@ -98,6 +98,7 @@ const Experience = () => {
                                 <small className='text-light'>Experienced</small>
                             </div>
                         </article>
+
                         <article className='experience__details'>
                             <BsPatchCheckFill className='experience__details-icon' />
                             <div>
@@ -105,6 +106,7 @@ const Experience = () => {
                                 <small className='text-light'>Experienced</small>
                             </div>
                         </article>
+
                         <article className='experience__details'>
                             <BsPatchCheckFill className='experience__details-icon' />
                             <div>
@@ -112,13 +114,14 @@ const Experience = () => {
                                 <small className='text-light'>Experienced</small>
                             </div>
                         </article>
+
                         <article className='experience__details'>
                             <BsPatchCheckFill className='experience__details-icon' />
                             <div>
                                 <h4>Docker, Docker-compose</h4>
-                                <small className='text-light'>intermediate</small>
+                                <small className='text-light'>Intermediate</small>
                             </div>
-                        </article>  
+                        </article>
                     </div>
                 </div>
             </div>
